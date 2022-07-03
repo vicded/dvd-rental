550031e790d11e07ae65d949756dd8dc06310f6f
victor <victordelgadodavalos@gmail.com>
2022-07-03 17:07:21 -0500

    created superuser for admin, tested Actors model on admin window

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Actors
+
+
 # Create your views here.
+def actor_template(request):
+    return HttpResponse('This is going to be my template')
+
--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from my_app.views import actor_template
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app/', actor_template),
 ]
