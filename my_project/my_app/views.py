506a8fc2309776f4b75865579c8da186a04463bd
victor <victordelgadodavalos@gmail.com>
2022-07-03 22:29:57 -0500

    added response and api_view decorator to my_app views for better handling and redability in the api code. added post option for the API

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,11 +1,23 @@
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Customer
 from .serializers import CustomerSerializer
 
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
 def customer_template(request):
-    customers = Customer.objects.all()
-    serializer = CustomerSerializer(customers, many=True)
-    return JsonResponse({'customers': serializer.data})
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return JsonResponse({'customers': serializer.data})
+
+    if request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
